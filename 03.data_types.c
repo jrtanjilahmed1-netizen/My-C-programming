5c95ffe938232d912ea51901eab98c5561634584
jrtanjilahmed1-netizen <jrtanjilahmed1@gmail.com>
2026-04-19 00:50:04 +0600

    Add 03.data_types.c with basic data type examples

---
+++ 03.data_types.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Integer type
+    int age = 18;
+
+    // Float type
+    float height = 5.9;
+
+    // Double type
+    double pi = 3.1415926535;
+
+    // Character type
+    char grade = 'A';
+
+    // Output
+    printf("Integer (age): %d\n", age);
+    printf("Float (height): %.2f\n", height);
+    printf("Double (pi): %.10lf\n", pi);
+    printf("Character (grade): %c\n", grade);
+
+    return 0;
+}
